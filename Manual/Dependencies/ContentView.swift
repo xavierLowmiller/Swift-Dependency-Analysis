b474af122d860021d66d2fe2192e9362b5185a58
Xavier Lowmiller <xaver.lohmueller@me.com>
2020-05-26 19:13:20 +0200

    Create empty projects

---
+++ Manual/Dependencies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dependencies
+//
+//  Created by Xaver Lohmüller on 26.05.20.
+//  Copyright © 2020 Xaver Lohmüller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
